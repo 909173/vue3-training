ead7da39715b0d2f30d400ba0c4daa9fe07ff671
oba <oba-mitsuki@ksc.co.jp>
2023-09-07 17:30:57 +0900

    day1 esLint hands on

--- ts/src/2.Linterの設定/fixESLint.ts
+++ ts/src/2.Linterの設定/fixESLint.ts
@@ -1,13 +1,17 @@
 export class User {
-  constructor(public name: number, public id: number) {}
+  constructor(public name: number, public id: number) {
+    this.createUser()
+  }
+
+  createUser() {
+    console.log("user created!")
+  }
 }
 
 export const main = (bool: boolean, bool2: boolean) => {
   if (bool) {
     console.log("bool is true")
-  } else {
-    if (bool2) {
-      console.log("bool is false, but bool2 is ture")
-    }
+  } else if (bool2) {
+    console.log("bool is false, but bool2 is true")
   }
 }
--- ts/src/2.Linterの設定/ignoreESLintTest.ts
+++ ts/src/2.Linterの設定/ignoreESLintTest.ts
@@ -1,7 +1,8 @@
 export class User {
+  // eslint-disable-next-line no-useless-constructor
   constructor(public name: number, public id: number) {}
 }
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const hogehoge = {
   user: "user"
 }
