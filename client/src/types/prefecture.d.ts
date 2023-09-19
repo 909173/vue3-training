66d9ff33aacb692384e8d52ce5b3e39d8e9d874f
55005 <saginuma-koki@ksc.co.jp>
2023-09-19 15:12:11 +0900

    .envファイル作成

--- client/src/types/prefecture.d.ts
+++ client/src/types/prefecture.d.ts
@@ -1,7 +1,7 @@
 // TODO: API定義書を参考に型定義すること
 export type Prefecture = {
-    prefCode: number
-    prefName: String
+  prefCode: number
+  prefName: string
 }
 // TODO: Prefecture型にインターセクション型を利用して、チェックボックスのbooleanも追加すること
 export type PrefectureDisplay = {
